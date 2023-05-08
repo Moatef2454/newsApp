103db6f047818f029334d7f00b0ee1f3d4889eff
mohamed atef <86878037+Moatef2454@users.noreply.github.com>
2023-05-08 03:15:33 +0300

    edit2

--- lib/modules/news_app_screens/web_screen.dart
+++ lib/modules/news_app_screens/web_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -8,8 +10,17 @@ class webScreen extends StatelessWidget {
   webScreen({super.key, required this.url});
   @override
   Widget build(BuildContext context) {
+    var controller = TextEditingController(text: '$url');
    return Scaffold(
-     appBar: AppBar(),
+     appBar: AppBar(
+       title: TextFormField(
+         onFieldSubmitted: (value) => Navigator.push(context, MaterialPageRoute(builder: (context) => webScreen(url: controller.text),)),
+         controller: controller,
+         decoration: InputDecoration(
+          border: InputBorder.none,
+         ),
+       ),
+     ),
      body: WebView(
        initialUrl: url,
      ),
